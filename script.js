16dd1a1b9d98fa21a2af85b37f273b86a2039004
jAmWhisk <whisk3yjam@gmail.com>
2022-01-22 15:51:18 -0500

    current version

--- script.js
+++ script.js
@@ -1,41 +1,46 @@
 var currentDayEl = document.getElementById('currentDay');
-var eventEl = document.getElementById('event');
-var saveBtnEl = document.getElementById('btn');
-var hour10 = document.getElementsByClassName('.hour10');
+
 
 var today = new Date();
 var currentTime = today.getHours();
 
 
-// display current day and date
-var time = moment().format("dddd, MMMM Do, YYYY");
-currentDayEl.textContent = time;
+// // display current day and date
+// var time = moment().format("dddd, MMMM Do, YYYY");
+// currentDayEl.textContent = time;
 
 
-// display different color for past, present, and future hours
-for (let i = 9; i < 18; i++) { 
-    let timeCol = document.getElementById(i);
-    if (i < currentTime) { 
-        timeCol.setAttribute('class', 'past');
+// // display different color for past, present, and future hours
+// for (let i = 9; i < 18; i++) { 
+//     let timeCol = document.getElementById(i);
+//     if (i < currentTime) { 
+//         timeCol.setAttribute('class', 'past');
         
-    } else if (i > currentTime) {
-        timeCol.setAttribute('class', 'future');
+//     } else if (i > currentTime) {
+//         timeCol.setAttribute('class', 'future');
         
-    } else {
-        timeCol.setAttribute('class', 'present');
+//     } else {
+//         timeCol.setAttribute('class', 'present');
 
-    }
-}
+//     }
+// }
+
+
+$(document).ready(function(){
+
+    $('.saveBtn').on('click', function () {
+        var text = $(this).siblings(".description").val();
+        var date = $(this).parent().attr("id");
+        console.log(text);
+        console.log(date);
+        localStorage.setItem(date, text);
+    });
+
+    $("#hour_9 .description").val(localStorage.getItem("hour_9"));
+    $("#hour_10 .description").val(localStorage.getItem("hour_10"));
+
+  });
 
 
 
-function save() {
-    let event = eventEl.value;
-    let date = hour10.attributes;
-    console.log(date);
-    localStorage.setItem(date, event);
-    var getHour10 = localStorage.getItem('hour_10');
-    hour10.value = getHour10;
-}
 
-saveBtnEl.onclick = save;
