700d5f6c4c2fdaa2649504a64c01a1e49a117136
jAmWhisk <whisk3yjam@gmail.com>
2022-01-22 14:20:36 -0500

    use moment.js to display time

--- script.js
+++ script.js
@@ -1,24 +1,41 @@
 var currentDayEl = document.getElementById('currentDay');
+var eventEl = document.getElementById('event');
+var saveBtnEl = document.getElementById('btn');
+var hour10 = document.getElementsByClassName('.hour10');
 
 var today = new Date();
-var day = today.getDay();
-var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
-var month = today.getMonth();
-var monthlist = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-var date = today.getDate();
+var currentTime = today.getHours();
 
 
-if (date === 1 || date === 11 || date === 21 || date === 31) { 
-    currentDayEl.textContent = daylist[day] + ", " + monthlist[month] + " " + date + "st";
+// display current day and date
+var time = moment().format("dddd, MMMM Do, YYYY");
+currentDayEl.textContent = time;
 
-} else if (date === 2 || date === 12 || date === 22) {
-    currentDayEl.textContent = daylist[day] + ", " + monthlist[month] + " " + date + "nd";
 
-} else if (date === 3 || date === 13 || date === 23) {
-    currentDayEl.textContent = daylist[day] + ", " + monthlist[month] + " " + date + "rd";
+// display different color for past, present, and future hours
+for (let i = 9; i < 18; i++) { 
+    let timeCol = document.getElementById(i);
+    if (i < currentTime) { 
+        timeCol.setAttribute('class', 'past');
+        
+    } else if (i > currentTime) {
+        timeCol.setAttribute('class', 'future');
+        
+    } else {
+        timeCol.setAttribute('class', 'present');
 
-} else {
-    currentDayEl.textContent = daylist[day] + ", " + monthlist[month] + " " + date + "th";
+    }
 }
 
 
+
+function save() {
+    let event = eventEl.value;
+    let date = hour10.attributes;
+    console.log(date);
+    localStorage.setItem(date, event);
+    var getHour10 = localStorage.getItem('hour_10');
+    hour10.value = getHour10;
+}
+
+saveBtnEl.onclick = save;
